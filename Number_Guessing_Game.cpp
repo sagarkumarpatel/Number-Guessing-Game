3bca63c12a3131d86855a0de46d941c701a78daa
sagarkumarpatel <sagarpatel98212@gmail.com>
2025-03-12 11:47:03 +0530

    This is my first commit, i just initilize the varibale like number, choice, option, guessing_number and one global variable count(which helps in tracking my count in how many count i guess the exact guessing_number)

---
+++ Number_Guessing_Game.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+     int count;
+int main(){
+    int number,choice;
+    string option;
+    int guessing_number=50;
+    cout<<"Welcome to the Number guessing game!"<<endl;
+    cout<<"I have to choose a number between  1 to 100. Try to guess it!"<<endl;
+return 0;
+}
