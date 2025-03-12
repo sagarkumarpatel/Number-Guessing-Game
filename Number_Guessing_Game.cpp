cf3b8dbf921433fbaa251b01d27d059f49f6cc5a
sagarkumarpatel <sagarpatel98212@gmail.com>
2025-03-12 12:00:10 +0530

    I add a function like, you want to play or not, if yes then there is option just chose and play

--- Number_Guessing_Game.cpp
+++ Number_Guessing_Game.cpp
@@ -1,11 +1,39 @@
 #include<iostream>
 using namespace std;
      int count;
+  void Guessing_game(int number,int guessing_number);
 int main(){
     int number,choice;
     string option;
     int guessing_number=50;
     cout<<"Welcome to the Number guessing game!"<<endl;
     cout<<"I have to choose a number between  1 to 100. Try to guess it!"<<endl;
+   do{
+    cout<<"1)Start to Play the Guessing Game"<<endl;
+    cout<<"2)option to play or not"<<endl;
+    cout<<"enter your choice: ";
+    cin>>choice;
+    switch(choice){
+        case 1:
+        cout<<"1)Enter your guessing number: ";
+        cin>>number;
+        Guessing_game(number,guessing_number);
+        cout<<"\n----------------------------------------"<<endl;
+        break;
+        case 2:
+        cout<<"Do you want to play again?(yes/no)"<<endl;
+        cout<<"Enter your option: "<<endl;
+        cin>>option;
+        if(option == "yes"){
+           Guessing_game(number,guessing_number);
+           cout<<"\n------------------------------------------"<<endl;
+        }
+        else{
+            cout<<"Thanks for playing! Goodbye!"<<endl;
+        }
+        break;
+    }
+   }while(choice!=3);
+   
 return 0;
 }
