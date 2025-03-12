6fa4bd2e48547d48425d2c25ec0181198fd2424e
sagarkumarpatel <sagarpatel98212@gmail.com>
2025-03-12 12:24:04 +0530

    update my previous code, and make it more efficient and debugg the previous count

--- Number_Guessing_Game.cpp
+++ Number_Guessing_Game.cpp
@@ -1,54 +1,60 @@
 #include<iostream>
 using namespace std;
-     int count;
-  void Guessing_game(int number,int guessing_number){
-    if(number!=guessing_number){
-     count++;
+
+int count;
+
+void Guessing_game(int number, int guessing_number) {
+    if (number != guessing_number) {
+        count++;
+        if (number > guessing_number) {
+            cout << "Too high! Try again." << endl;
+        } else {
+            cout << "Too low! Try again." << endl;
+        }
+    } else {
+        cout << "Congratulations! You guessed the number in " << count << " attempts!" << endl;
     }
-    if(number==guessing_number){
-     cout<<"Congratulations! You guessed the number in "<<count<<" attempts!"<<endl;
-    }
-    else{
-     if(number>guessing_number){
-         cout<<"Too high! Try again."<<endl;
-     }
-     else{
-         cout<<"Too Low! Try again."<<endl;
-     }
-    }
-  }
-int main(){
-    int number,choice;
+}
+
+int main() {
+    int number, choice;
     string option;
-    int guessing_number=50;
-    cout<<"Welcome to the Number guessing game!"<<endl;
-    cout<<"I have to choose a number between  1 to 100. Try to guess it!"<<endl;
-   do{
-    cout<<"1)Start to Play the Guessing Game"<<endl;
-    cout<<"2)option to play or not"<<endl;
-    cout<<"enter your choice: ";
-    cin>>choice;
-    switch(choice){
-        case 1:
-        cout<<"1)Enter your guessing number: ";
-        cin>>number;
-        Guessing_game(number,guessing_number);
-        cout<<"\n----------------------------------------"<<endl;
-        break;
-        case 2:
-        cout<<"Do you want to play again?(yes/no)"<<endl;
-        cout<<"Enter your option: "<<endl;
-        cin>>option;
-        if(option == "yes"){
-           Guessing_game(number,guessing_number);
-           cout<<"\n------------------------------------------"<<endl;
+    int guessing_number = 50; // You can change this number for more fun!
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have chosen a number between 1 to 100. Try to guess it!" << endl;
+
+    do {
+        count = 0; // Reset the count for each new game
+        cout << "1) Start to Play the Guessing Game" << endl;
+        cout << "2) Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            do {
+                cout << "Enter your guessing number (1-100): ";
+                cin >> number;
+
+                if (number < 1 || number > 100) {
+                    cout << "Invalid input! Please enter a number between 1 and 100." << endl;
+                    continue;
+                }
+
+                Guessing_game(number, guessing_number);
+
+            } while (number != guessing_number);
+
+            cout << "Do you want to play again? (yes/no): ";
+            cin >> option;
+
+        } else if (choice == 2) {
+            cout << "Thanks for playing! Goodbye!" << endl;
+            break;
+        } else {
+            cout << "Invalid choice! Please try again." << endl;
         }
-        else{
-            cout<<"Thanks for playing! Goodbye!"<<endl;
-        }
-        break;
-    }
-   }while(choice!=3);
-   
-return 0;
+
+    } while (option == "yes");
+
+    return 0;
 }
